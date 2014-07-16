13a51163453db56deae6819b66a00a42db23efdb
darwin <darwinleonardo1986@gmail.com>
2014-07-16 14:45:58 -0430

    nuevo bien

--- script/script.sh
+++ script/script.sh
@@ -3,12 +3,12 @@ fecha=$(date +"%m-%d-%Y-%T")
 
 echo *********************INICIO SCRIPT.SH******************
 
-echo $fecha
+
+
+cd /home/academia/ 
 
 cd respaldo
 
-cd /home/academia/ respaldo
-
-cp -R /home/academia/control/ /home/academia/respaldo/respaldo $fecha
+cp -R  /home/academia/control /home/academia/respaldo/respaldo$fecha
 
 echo *********************FIN SCRIPT************************
